dda51944ae06c05afd2bd409c222e5e43ce3c6b4
Bryan Leonard <bryan@forcequit.io>
2025-03-20 09:54:32 -0400

    Add some timers and a results modal starter code

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import Player from './components/Player.jsx';
+import TimerChallenge from './components/TimerChallenge.jsx';
 
 function App() {
   return (
     <>
       <Player />
-      <div id="challenges"></div>
+      <div id="challenges" className="challenges">
+        <TimerChallenge title="Easy 👶🏽" targetTime={1} />
+        <TimerChallenge title="Medium " targetTime={5} />
+        <TimerChallenge title="Kinda spicy 🌶️" targetTime={10} />
+        <TimerChallenge title="Pros only" targetTime={15} />
+      </div>
     </>
   );
 }
--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,11 +1,49 @@
+import { useState, useRef } from 'react';
+
 export default function Player() {
+  const playerName = useRef();
+  const [givenPlayerName, setPlayerName] = useState(null);
+
+  function handleClick() {
+    setPlayerName (playerName.current.value);
+  }
+
   return (
-    <section id="player">
-      <h2>Welcome unknown entity</h2>
+    <section id="player" className="player">
+      <h2>Welcome, {givenPlayerName ?? 'unknown entity'}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input ref={playerName} type="text" />
+        <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
   );
 }
+
+
+
+// v1
+// import { useState } from 'react';
+
+// export default function Player() {
+//   const [playerName, setPlayerName] = useState('');
+//   const [sub, setSub] = useState(false);
+
+//   function handleChange(evt) {
+//     setPlayerName(evt.target.value);
+//   }
+
+//   function handleClick() {
+//     setSub(true);
+//   }
+
+//   return (
+//     <section id="player">
+//       <h2>Welcome, {sub ? playerName : 'unknown entity'}</h2>
+//       <p>
+//         <input type="text" onChange={handleChange} value={playerName} />
+//         <button onClick={handleClick}>Set Name</button>
+//       </p>
+//     </section>
+//   );
+// }
+
