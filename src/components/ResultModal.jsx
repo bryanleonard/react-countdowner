a6712a63fc3408c0f338a81849ba3168885d1290
Bryan Leonard <bryan@forcequit.io>
2025-03-20 20:50:53 -0400

    Adding modal response to fails

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -1,5 +1,5 @@
 
-// React 19+ solution
+// React 19+ solution using refs.
 // export default function ResultModal({ref, result, targetTime}) {
 	
 // 	return (
@@ -15,13 +15,23 @@
 // }
 
 // React <19 solution
-import { forwardRef } from 'react';
+import { forwardRef, useImperativeHandle, useRef } from 'react';
+
 const ResultModal = forwardRef(function ResultModal({result, targetTime}, ref) {
-	
+	const dialog = useRef();
+
+	useImperativeHandle(ref, () => {
+		return {
+			open() {
+				dialog.current.showModal();
+			}
+		};
+	});
+
 	return (
-		<dialog ref={ref}className="result-modal">
+		<dialog ref={dialog} className="result-modal">
 			<h2>You {result}!</h2>
-			<p>The target time was <strong> {targetTime} seconds.</strong></p>
+			<p>The target time was <strong> {targetTime} second{targetTime > 1 ? 's' : ''}.</strong></p>
 			<p>You stopped the timer with <strong>x seconds remaining.</strong></p>
 			<form method="dialog">
 				<button>Close</button>
